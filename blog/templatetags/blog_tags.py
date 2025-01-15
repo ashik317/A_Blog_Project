4ffc1478c528651a6c969c85f70e41aa713d4555
Ashikur Rahman Likhon <mdarlikhon317@gmail.com>
2025-01-15 16:09:46 +0600

    Creating a template filter to support Markdown syntax

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,8 +1,8 @@
-from itertools import count
-
+import markdown
 from django import template
 from ..models import Post
-
+from django.db.models import Count
+from django.utils.safestring import mark_safe
 register = template.Library()
 @register.simple_tag
 def total_posts():
@@ -12,3 +12,13 @@ def total_posts():
 def show_latest_posts(count=5):
  latest_posts = Post.published.order_by('-publish')[:count]
  return {'latest_posts': latest_posts}
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(
+        total_comments=Count('comments')
+    ).order_by('-total_comments')[:count]
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
