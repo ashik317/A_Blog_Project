bacaaf2f9f105953f6e8f037419b0d8b4daea663
Ashikur Rahman Likhon <mdarlikhon317@gmail.com>
2025-01-16 17:28:27 +0600

    Adding full-text search to the blog

--- A_Blog_Project/settings.py
+++ A_Blog_Project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.sitemaps',
+    'django.contrib.postgres',
     'taggit',
     'blog.apps.BlogConfig',
 ]
@@ -82,8 +83,12 @@ WSGI_APPLICATION = 'A_Blog_Project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': '5432',
     }
 }
 
